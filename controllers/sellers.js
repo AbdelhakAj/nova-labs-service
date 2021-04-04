ab12c0b4f4ad8de1fc02c29c8327b2eaaf58b07f
Abedelhak <abdelhak@groupado.com>
2021-04-04 14:25:35 +0100

    add getSellerById endpoint

--- controllers/sellers.js
+++ controllers/sellers.js
@@ -3,6 +3,7 @@ const express = require("express")
 const { check, validationResult } = require('express-validator')
 
 const sellers = require("../services/sellers")
+const timeSlots = require("../services/timeSlots")
 
 const router = express.Router()
 
@@ -28,6 +29,25 @@ router.get("/",
     }
 );
 
+// getSellerById(sellerId)
+router.get("/:sellerId",
+    [
+        check('sellerId', 'sellerId is required')
+            .exists()
+    ],
+    async (req, res) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+        const { sellerId } = req.params
+        const { date } = req.query
+
+        const response = await sellers.getSellerById(sellerId, date).catch(error => res.status(500).json({ errors: error.toString() }))
+        res.send(response)
+    }
+);
+
 // addSellers(body)
 router.post("/",
     [
@@ -39,11 +59,51 @@ router.post("/",
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() })
         }
-        const {
-            fullName
-        } = req.body
+        const { fullName } = req.body
 
         const response = await sellers.addSeller(fullName).catch(error => res.status(500).json({ error }))
+        res.send(response)
+    }
+);
+
+// addTimeSlotToSeller(sellerId, body)
+router.post("/:sellerId/timeslots",
+    [
+        check('date', 'date should be a valid Date')
+            .isISO8601(),
+        check('start', 'start should be a String')
+            .isString(),
+        check('end', 'end should be a String')
+            .isString()
+    ],
+    async (req, res) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+        const { date, start, end } = req.body
+        const { sellerId } = req.params
+
+        const response = await timeSlots.addTimeSlotToSeller(sellerId, { date, start, end }).catch(error => res.status(500).json({ error }))
+        res.send(response)
+    }
+);
+
+// updateTimeSlotBySeller(sellerId, timeSlotsId, isBooked)
+router.put("/:sellerId/timeslots/:timeSlotsId",
+    [
+        check('isBooked', 'isBooked should be Boolean')
+            .isBoolean(),
+    ],
+    async (req, res) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+        const { isBooked } = req.body
+        const { sellerId, timeSlotsId } = req.params
+
+        const response = await timeSlots.updateTimeSlotBySeller(sellerId, timeSlotsId, isBooked).catch(error => res.status(500).json({ error }))
         res.send(response)
     }
 );
