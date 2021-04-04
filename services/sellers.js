fe7e76ae15794c0648acb875b5e287ff68f72432
Abedelhak <abdelhak@groupado.com>
2021-04-04 14:26:54 +0100

    add getSellerById service

--- services/sellers.js
+++ services/sellers.js
@@ -1,8 +1,9 @@
 const Seller = require('../models/seller')
+const timeSlots = require("../services/timeSlots")
 const errorHandler = require('../utils/errorHandler')
 
 const getSellers = (page, perPage, q) => {
-    const query = q ? {"fullName" : {$regex : `.*${q}.*`}} : {}
+    const query = q ? { "fullName": { $regex: `.*${q}.*` } } : {}
     const skip = page * perPage
     const limit = +perPage
     const response = Seller
@@ -15,14 +16,30 @@ const getSellers = (page, perPage, q) => {
     return response
 }
 
-const addSeller = (fullName) => {
-    return Seller
-    .create({fullName}).catch(error => {
-        errorHandler(error, '[sellers] error in services/sellers addSeller')
+const getSellerById = async (sellerId, date) => {
+    const sellerResponse = await Seller.findById(
+        sellerId
+    ).catch(error => {
+        errorHandler(error, '[sellers] error in services/sellers getSellerById')
     })
+
+    const timeSlotsResponse = await timeSlots.getTimeSlotsByIds(sellerResponse.timeSlots, date)
+    const response = {...sellerResponse.toObject(), timeSlots: timeSlotsResponse}
+
+    return response
+}
+
+const addSeller = (fullName) => {
+    const response = Seller
+        .create({ fullName }).catch(error => {
+            errorHandler(error, '[sellers] error in services/sellers addSeller')
+        })
+
+    return response
 }
 
 module.exports = {
     getSellers,
+    getSellerById,
     addSeller
 };
